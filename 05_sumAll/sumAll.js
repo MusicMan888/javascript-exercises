3e45461771780983ab37b8188159580f6bbe2faa
Chris Daykin <103705442+MusicMan888@users.noreply.github.com>
2023-05-25 20:35:12 -0400

    Add two more problem solutions
    This adds two more solutions, sumAll was the provided solution,
    leapYear is mine - based on a truth table

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
 
+// I was stuck on this for well over a week - the number methods were not taught - at a certain point you need to 
+// move on
+
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+console.log(sumAll(1, 4000));
 
 // Do not edit below this line
 module.exports = sumAll;
