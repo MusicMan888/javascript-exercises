7a0425c15723b90c7b2d8e60472575767a8014cf
Chris Daykin <103705442+MusicMan888@users.noreply.github.com>
2023-06-05 20:37:38 -0400

    completes Javascript exercises using node.js
    This completes the exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,27 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempF)
+{
+  let result = (tempF - 32) * (5/9);
+  if (result % 1 === 0)
+  {
+    return result;
+  } else
+  {
+    return Number(result.toFixed(1));
+  }
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempC)
+{
+  let result = (tempC * (9/5)) + 32;
+  if (result % 1 === 0)
+  {
+    return result;
+  } else
+  {
+    return Number(result.toFixed(1));
+    
+  }
+}
 
 // Do not edit below this line
 module.exports = {
